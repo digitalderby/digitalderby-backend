6505fad1dc98a183f7eb9310106865d56f40dd9b
Will Fowlkes <williamowenfowlk@gmail.com>
2024-04-26 18:06:54 -0400

    Add authentication middleware

--- src/auth/authentication.ts
+++ src/auth/authentication.ts
@@ -1,7 +1,18 @@
 import { Handler, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
 
-export async function loggedInAsUser(req: Request, res: Response, next: Handler) {
-    // TODO
+export async function loggedInAsUser(req: any, res: Response, next: any) {
+    try {
+        let token = req.get('Authorization') || req.query.token || req.body.token
+        if (!token) { throw 'Not Logged In' }
+        token = token.replace('Bearer ', '')
+
+        const payload = jwt.verify(token, process.env.AUTH_SECRET || "")
+        req.payload = payload
+        next()
+    } catch(error) {
+        res.status(401).json('Must be logged in to perform this action')
+    }
 }
 
 export async function loggedInAsAdmin(req: Request, res: Response, next: Handler) {
