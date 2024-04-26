e791126737790e38d8cf37ac564a560cf697b11e
Will Fowlkes <williamowenfowlk@gmail.com>
2024-04-26 17:55:34 -0400

    Add middleware and additional packages

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,38 @@ import { createServer } from 'node:http'
 import { GameServer } from './game/gameServer.js'
 import { GameDatabase } from './game/gameDatabase.js'
 
+import cors from 'cors'
+import logger from 'morgan'
+
 import HorseRouter from './routes/horses.js'
 import UserRouter from './routes/users.js'
 import AdminRouter from './routes/admin.js'
 import RaceRouter from './routes/races.js'
 import HttpInterfaceRouter from './routes/http.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 const server = createServer(app)
 
 const PORT = process.env.PORT || 3000
 
+const corsSettings = {
+    credentials: true,
+    origin: [
+        'http://localhost:5173',
+    ]
+}
+
 let db = GameDatabase.create()
 let gameServer = new GameServer(db, server)
+
+app.use(logger('dev'))
+app.use(cors(corsSettings))
+app.use(bodyParser.json())
+
+app.get('/', (_req, res) => {
+    res.json({message: "this is the index route, the server is working"})
+})
 
 app.use('/horses', HorseRouter)
 app.use('/users', UserRouter)
