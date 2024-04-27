7e24a8a1781914620e5e65bcadddf77057b99fc1
Fekinox <williamowenfowlk@gmail.com>
2024-04-27 19:12:23 -0400

    Add user querying routes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response } from "express"
+import { sendJSONError } from "../errorHandler.js"
+import User from "../models/User.js"
 
 export async function getAllUsers(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({
-        message: 'Server settings'
-    })
+    try {
+        const usernames = await User.find(
+            {},
+            'username'
+        )
+        res.status(200).json(usernames)
+    } catch (error) {
+        sendJSONError(res, 500, `Internal error retrieving users: ${error}`)
+    }
 }
 
 export async function getUserById(req: Request, res: Response, next: NextFunction) {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,7 @@ import { getAllUsers, getUserById } from '../controllers/users.js'
 
 const router = express.Router()
 
-router.post('/', getAllUsers)
-router.post('/:uname', getUserById)
+router.get('/', getAllUsers)
+router.get('/:uname', getUserById)
 
 export default router
