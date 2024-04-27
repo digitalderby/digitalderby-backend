0c8714930357bdca0d65c86cab5c46e6e6196fec
Fekinox <williamowenfowlk@gmail.com>
2024-04-27 19:37:15 -0400

    Add some jsdocs

--- src/random/random.ts
+++ src/random/random.ts
@@ -1,12 +1,28 @@
-/** Returns a random integer in the range (lo, hi) inclusive. */
+/**
+ * Returns a random integer in the inclusive integer range [lo, hi].
+ *
+ * @param {number} lo
+ * @param {number} hi
+ * @returns {number}
+ */
 export function randRange(lo: number, hi: number): number {
     return Math.floor(lo + Math.random() * (hi - lo + 1))
 }
 
-/** Returns a random element from the range described by the two-value array `bounds`. */
+/**
+ * Given a two-element array `bounds` of two numbers defining an inclusive
+ * integer range, return a random element inside that range.
+ *
+ * @param {Array} bounds
+ * @returns {number}
+ */
 export function randFromBounds(bounds: Array<number>): number { return randRange(bounds[0], bounds[1]) }
 
-/** Shuffles the array in-place using the Fisher-Yates algorithm. */
+/**
+ * Shuffles the given array in-place using the Fisher-Yates algorithm.
+ *
+ * @param {Array} arr
+ */
 export function shuffle(arr: Array<any>) {
     // Fisher-Yates shuffle
     for (let i = 0; i < arr.length-1; i++) {
@@ -17,9 +33,14 @@ export function shuffle(arr: Array<any>) {
     }
 }
 
-/** Returns a random subset of [0, n) of size k. Will return null if k > n.
-* The order of the elements in the subset is randomized.
-*/
+/**
+ * Gets a random set of numbers of size k in the range 0, 1, ... n-1. Returns
+ * null if k > n.
+ *
+ * @param {number} n
+ * @param {number} k
+ * @returns {Array<number> | null}
+ */
 export function randomIndicesNoReplacement(n: number, k: number): Array<number> | null {
     if (n < k) { return null }
     let arr = Array.from({length: n}, (_, i) => i)
