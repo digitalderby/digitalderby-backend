941d47c633d78a61515c4e14f65944d2d13bb02e
Fekinox <williamowenfowlk@gmail.com>
2024-04-27 12:18:35 -0400

    Add password authentication

---
+++ src/auth/password.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+
+const SALT_ROUNDS = 20
+
+export async function saltPassword(password: string): Promise<string> {
+    if (password.length === 0) {
+        throw new Error('Password must be a nonempty string')
+    }
+
+    return await bcrypt.hash(password, await bcrypt.genSalt(SALT_ROUNDS))
+}
+
+export async function verifyPassword(target: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(target, hash)
+}
