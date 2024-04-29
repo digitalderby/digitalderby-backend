0123efe2a894e95990c0ef4797660359c2af6667
Will Fowlkes <williamowenfowlk@gmail.com>
2024-04-29 16:32:31 -0400

    Remove extraneous type

--- src/controllers/horses.ts
+++ src/controllers/horses.ts
@@ -1,11 +1,10 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { Horse } from "../models/Horse.js";
 import { sendJSONError } from "../errorHandler.js";
 
 export async function getAllHorses(
     req: Request,
     res: Response,
-    next: NextFunction
 ) {
   try {
     const horses = await Horse.find({}).lean();
@@ -15,15 +14,20 @@ export async function getAllHorses(
   }
 }
 
-export async function getHorseById(req: Request, res: Response, next: NextFunction) {
+export async function getHorseById(req: Request, res: Response) {
+  try {
+    const horse = await Horse.findById(req.params.id)
+    if (!horse) {
+      return sendJSONError(res, 404, `Horse not found`);
+    }
+    res.status(200).json(horse);
+  } catch (error) {
+    sendJSONError(res, 500, `Internal error retrieving horse: ${error}`);
+  }
+}
+
+export async function getHorsesLastGames(req: Request, res: Response) {
     res.status(200).json({
         message: 'Server settings'
     })
 }
-
-export async function getHorsesLastGames(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({
-        message: 'Server settings'
-    })
-}
-
