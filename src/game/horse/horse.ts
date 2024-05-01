ba19e4e9e7bb8cb7180a35e4fdaf84c2520f5fe9
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-01 14:40:19 -0400

    Implement basic stamina consumption, lo/mid/high speed modes

--- src/game/horse/horse.ts
+++ src/game/horse/horse.ts
@@ -1,8 +1,13 @@
 import { HorseSpec, } from "../../models/Horse.js";
 
-export const SPEED_BOUNDS = [200, 250]
+export const LOW_SPEED_BOUNDS = [60, 70]
+export const MID_SPEED_BOUNDS = [160, 170]
+export const HIGH_SPEED_BOUNDS = [200, 220]
 export const ACCELERATION_BOUNDS = [1,9]
 export const STAMINA_BOUNDS = [500, 550]
+
+export const DECELERATION = 12
+export const MODE_DURATION = 4000
 
 function interpolate(specStatValue: number, bounds: Array<number>): number {
     const normalized = (specStatValue - 3)/17
@@ -12,16 +17,18 @@ function interpolate(specStatValue: number, bounds: Array<number>): number {
 export class InternalHorse {
     spec: HorseSpec
 
-    topSpeed: number;
+    highSpeed: number;
+    midSpeed: number;
+    lowSpeed: number;
     stamina: number;
     acceleration: number;
-
-    mode: 'low' | 'mid' | 'high' = 'mid'
 
     constructor(horseSpec: HorseSpec) {
         this.spec = horseSpec
 
-        this.topSpeed = interpolate(horseSpec.stats.topSpeed, SPEED_BOUNDS)
+        this.highSpeed = interpolate(horseSpec.stats.topSpeed, HIGH_SPEED_BOUNDS)
+        this.midSpeed = interpolate(horseSpec.stats.topSpeed, MID_SPEED_BOUNDS)
+        this.lowSpeed = interpolate(horseSpec.stats.topSpeed, LOW_SPEED_BOUNDS)
         this.acceleration = interpolate(horseSpec.stats.acceleration, ACCELERATION_BOUNDS)
         this.stamina = interpolate(horseSpec.stats.stamina, STAMINA_BOUNDS)
     }
--- src/game/raceState.ts
+++ src/game/raceState.ts
@@ -1,6 +1,51 @@
 import { RACE_LENGTH, SERVER_TICK_RATE_MS } from "../config/globalsettings.js"
-import { InternalHorse } from "./horse/horse.js"
+import { DECELERATION, InternalHorse, MODE_DURATION } from "./horse/horse.js"
 import { Race } from "./race.js"
+
+const lowPlacementFactors = [
+    1, 0.75, 0.5, 0.25
+]
+
+const highPlacementFactors = [
+    0.25, 0.5, 0.75, 1
+]
+
+function lowSpeedProb(staminaPercent: number, placement: number): number {
+    const staminaFactor =
+        (staminaPercent < 0.2)
+        ? 0.5 + ((0.2 - staminaPercent)/0.2)*0.5
+        : 0.25
+    const placementFactor = lowPlacementFactors[placement]
+    const timeFactor = SERVER_TICK_RATE_MS/(1000 * 20)
+    return staminaFactor * placementFactor * timeFactor
+}
+
+function highSpeedProb(staminaPercent: number, placement: number): number {
+    const staminaFactor =
+        (staminaPercent > 0.5)
+        ? 0.5 + ((staminaPercent - 0.5)/0.5)*0.5
+        : 0.25
+    const placementFactor = highPlacementFactors[placement]
+    const timeFactor = SERVER_TICK_RATE_MS/(1000 * 20)
+    return staminaFactor * placementFactor * timeFactor
+}
+
+function rollForModeBuff(staminaPercent: number, placement: number): {
+    mode: 'low' | 'high', duration: number
+} | null {
+    if (Math.random() < lowSpeedProb(staminaPercent, placement)) {
+        return {
+            mode: 'low',
+            duration: MODE_DURATION,
+        }
+    } else if (Math.random() < highSpeedProb(staminaPercent, placement)) {
+        return {
+            mode: 'high',
+            duration: MODE_DURATION,
+        }
+    }
+    return null
+}
 
 export class RaceState {
     horseStates: Array<HorseState> = []
@@ -20,17 +65,71 @@ export class RaceState {
     nextState(): RaceState {
         let next = new RaceState()        
         next.time = this.time + SERVER_TICK_RATE_MS
-        next.horseStates = this.horseStates.map((hs) => {
+        next.horseStates = this.horseStates.map((hs, i) => {
             if (hs.finishTime !== null) { return hs }
             let nextHs = new HorseState(hs.horse)
-            nextHs.currentSpeed = 
-                Math.min(hs.currentSpeed + hs.horse.acceleration,
-                         hs.horse.topSpeed)
+            const staminaPercent = hs.currentStamina / hs.horse.stamina
+            const currentPlacement = this.placement(i)
+
+            const targetSpeed =
+                (hs.speedMode === 'high')
+                ? hs.horse.highSpeed
+                : (hs.speedMode === 'mid')
+                  ? hs.horse.midSpeed
+                  : hs.horse.lowSpeed
+
+            // Move position by currentSpeed
             nextHs.position = hs.position + hs.currentSpeed
+
+            // If the horse reaches the end, set their finish time
             if (nextHs.position >= RACE_LENGTH) {
                 nextHs.position = RACE_LENGTH
                 nextHs.finishTime = next.time
             }
+
+            // Decelerate if we are faster than the target speed
+            if (nextHs.currentSpeed > targetSpeed) {
+                nextHs.currentSpeed =
+                    Math.max(hs.currentSpeed - DECELERATION,
+                             targetSpeed)
+            }
+
+            // Accelerate if we are behind the target speed
+            nextHs.currentSpeed = 
+                Math.min(hs.currentSpeed + hs.horse.acceleration,
+                         hs.horse.highSpeed)
+
+            // If in low mode, increase stamina by 12.5% max per second
+            // If in mid mode, decrease stamina proportional to top speed
+            // If in high mode, decrease stamina proportional to top speed but faster
+            const staminaChange =
+                Math.floor((SERVER_TICK_RATE_MS/1000) * ((hs.speedMode === 'low')
+                ? hs.horse.stamina/8
+                : (hs.speedMode === 'mid')
+                  ? -(hs.currentSpeed/100)
+                  : -(hs.currentSpeed/50)
+))
+            nextHs.currentStamina = Math.max(0, hs.currentStamina + staminaChange)
+
+            // If the horse is currently deciding to go at 'low' or 'high'
+            // speed for a time, decrease the buff duration
+            if (hs.modeBuff !== null && hs.modeBuff.duration > 0) {
+                nextHs.modeBuff = {
+                    mode: hs.modeBuff.mode,
+                    duration: hs.modeBuff.duration - SERVER_TICK_RATE_MS,
+                }
+            // If out of stamina, enter 'low' mode
+            } else if (nextHs.currentStamina <= 0) {
+                nextHs.modeBuff = {
+                    mode: 'low',
+                    duration: MODE_DURATION,
+                }
+            } else {
+                nextHs.modeBuff = rollForModeBuff(staminaPercent, currentPlacement)
+            }
+            // If there is no active buff, horse is moving at 'mid' speed-
+            // otherwise, at the speed defined by the mode buff
+            nextHs.speedMode = (nextHs.modeBuff === null) ? 'mid' : nextHs.modeBuff.mode
 
             return nextHs
         })
@@ -66,6 +165,10 @@ export class RaceState {
         })
     }
 
+    placement(horseIdx: number): number {
+        return this.rankings.findIndex((idx) => horseIdx === idx)
+    }
+
     raceOver() {
         return this.horseStates.every((hs) => hs.finishTime !== null)
     }
@@ -76,9 +179,18 @@ export class HorseState {
 
     position: number = 0
     currentSpeed: number = 0
+    currentStamina: number = 0
     finishTime: number | null = null
+
+    speedMode: 'low' | 'mid' | 'high' = 'mid'
+    modeBuff: {
+        mode: 'low' | 'high',
+        duration: number,
+    } | null = null
+
 
     constructor(horse: InternalHorse) {
         this.horse = horse
+        this.currentStamina = horse.stamina
     }
 }
