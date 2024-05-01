ab83da04825cd15c9d6759d0f0060f322e090084
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-01 16:20:27 -0400

    Fix package.json, remove js files

--- src/cors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    credentials: true,
-    origin: [
-        'http://localhost:5173',
-        process.env.FRONTEND_DEPLOYMENT_URL || 'http://localhost:5173',
-    ]
-};
--- src/routes/horses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var horses_js_1 = require("../controllers/horses.js");
-var router = express_1.default.Router();
-router.get('/', horses_js_1.getAllHorses);
-router.get('/:id', horses_js_1.getHorseById);
-router.get('/:id/lastGames', horses_js_1.getHorsesLastGames);
-exports.default = router;
