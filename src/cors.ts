8fb12383179eec5c18e506786d78b4142eaac04e
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-02 11:11:52 -0400

    Add debug logging for cors settings

--- src/cors.ts
+++ src/cors.ts
@@ -1,7 +1,11 @@
-export default {
+const cors = {
     credentials: true,
     origin: [
         'http://localhost:5173',
         process.env.FRONTEND_DEPLOYMENT_URL || 'http://localhost:5173',
     ]
 }
+
+console.log('cors:', cors)
+
+export default cors
