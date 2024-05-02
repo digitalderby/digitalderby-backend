bf8b57e31ce2ab14c23aa9d21621372ef710402b
vitrineofcode <tilers_unable_0n@icloud.com>
2024-05-02 10:57:00 -0400

    Addition of createUser and updateUser routes to routes folder

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,21 @@
-import express from 'express'
-import { deleteUser, getAllUsers, getUserByUsername } from '../controllers/users.js'
-import { loggedInAsAdmin } from '../auth/authentication.js'
+import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUserByUsername,
+  updateUser,
+  createUser,
+} from "../controllers/users.js";
+import { loggedInAsAdmin } from "../auth/authentication.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', getAllUsers)
-router.get('/:uname', getUserByUsername)
-router.delete('/:uname', loggedInAsAdmin, deleteUser)
+router.get("/", getAllUsers);
+router.get("/:uname", getUserByUsername);
 
-export default router
+router.post("/", createUser);
+
+router.patch("/:uname", loggedInAsAdmin, updateUser);
+router.delete("/:uname", loggedInAsAdmin, deleteUser);
+
+export default router;
