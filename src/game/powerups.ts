507d265bdc0744660898a2e8f6a7e6964da32871
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-03 16:58:15 -0400

    Lint

--- src/game/powerups.ts
+++ src/game/powerups.ts
@@ -6,16 +6,17 @@ import { Race } from './race.js';
 export type Powerup = (horse: InternalHorse, race: Race) => void;
 
 // Speed Boost Powerup
-export const applySpeedBoost: Powerup = (horse, race, boostAmount = 5) => { // Default boost amount is 5
-  horse.highSpeed += boostAmount; // Increase high speed 
-  horse.midSpeed += boostAmount * 0.75; // Mid speed increases 
-  horse.lowSpeed += boostAmount * 0.5; // Low speed increases 
+export const applySpeedBoost: Powerup = (horse, race, boostAmount = 5) => {
+  // Default boost amount is 5
+  horse.highSpeed += boostAmount; // Increase high speed
+  horse.midSpeed += boostAmount * 0.75; // Mid speed increases
+  horse.lowSpeed += boostAmount * 0.5; // Low speed increases
 };
 
 // Stamina Boost Powerup
-export const applyStaminaBoost: Powerup = (horse, race, boostAmount = 10) => { // Default boost amount is 10
+export const applyStaminaBoost: Powerup = (horse, race, boostAmount = 10) => {
+  // Default boost amount is 10
   horse.stamina += boostAmount; // Directly increase stamina
 };
 
 // Functions are not importted in any file
-// 
