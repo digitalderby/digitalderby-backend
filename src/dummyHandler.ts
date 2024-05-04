ec4c4300eef94fd9638040ece5595caa0bd6fae9
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-04 14:48:00 -0400

    Add routes for /races endpoint

--- src/dummyHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Handler, Request, Response } from 'express';
-
-export default async function dummyRouter(
-  req: Request,
-  res: Response,
-  next: Handler,
-) {
-  res.json({ endpoint: req.baseUrl });
-}
