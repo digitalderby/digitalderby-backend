68570c0828d926bf24407285b1196152ba67f079
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-06 09:25:48 -0400

    Remove js files

--- src/auth/tokens.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.markDeletedUser = exports.deletedUsers = void 0;
-exports.deletedUsers = new Set();
-function markDeletedUser(username) {
-    exports.deletedUsers.add(username);
-}
-exports.markDeletedUser = markDeletedUser;
--- src/time/time.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hrTimeMs = void 0;
-function hrTimeMs() {
-    var raw = process.hrtime.bigint();
-    return raw / 1000000n;
-}
-exports.hrTimeMs = hrTimeMs;
